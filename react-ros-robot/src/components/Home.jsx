b764367794a44702d2544dec198234f701bafc91
takumitomokawa <tomokawa.takumi163@mail.kyutech.jp>
2022-10-10 13:32:05 +0700

    add react-ros-robot

---
+++ react-ros-robot/src/components/Home.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import Connection from "./Connection";
+import ConnectionXArm from "./ConnectionXArm";
+import Teleoperation from "./Teleoperation";
+import RobotState from "./RobotState";
+import Map from "./Map";
+import Camera from "./Camera";
+import MarkerMovement from "./MarkerMovement";
+import { Row, Col, Container } from "react-bootstrap";
+
+class Home extends Component {
+    state = {
+    };
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <h1 className="text-center mt-3">Robot Control Page</h1>
+                    <Col>
+                            <Connection></Connection>
+                            <ConnectionXArm></ConnectionXArm>
+                    </Col>
+                    <Row>
+                        <Col>          
+                            <Teleoperation></Teleoperation>
+                        </Col>
+                        <Col>
+                            <MarkerMovement /> 
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Camera />
+                            <RobotState />
+                        </Col>
+                        <Col>
+                            <h4 className="mt-2">MAP</h4>
+                            <Map></Map>
+                        </Col>
+                    </Row>
+
+                    {/* <h1 className="text_center mt-3">Robot Control Page</h1>
+                    <Col>
+                            <Connection></Connection>
+                    </Col>
+                    <Col>
+                        <Col>          
+                            <Teleoperation></Teleoperation>
+                        </Col>
+                        <Row>
+                            <Col>
+                                <Camera />
+                                <RobotState />
+                            </Col>
+                            <Col>          
+                                <h4>MAP</h4>
+                                <Map></Map>
+                            </Col>
+                        </Row>
+                    </Col> */}
+
+                </Container>
+             
+            </div>
+        );
+    }
+
+}
+
+export default Home;
---
+++ react-ros-robot/src/scripts/config.js
@@ -0,0 +1,17 @@
+const Config = {
+  // ROSBRIDGE_SERVER_IP: "10.80.73.240",
+  ROSBRIDGE_SERVER_IP: "10.0.1.119",
+  ROSBRIDGE_SERVER_PORT: "9090",
+  XARM_ROSBRIDGE_SERVER_IP: "10.0.1.27",
+  XARM_ROSBRIDGE_SERVER_PORT: "9090",
+  RECONNECTION_TIMER: 3000,
+  CMD_VEL_TOPIC: "/cmd_vel",
+  MARKER_TOGGLE_TOPIC: "/marker_toggle",
+  ODOM_TOPIC: "/odom",
+  POSE_TOPIC: "/amcl_pose",
+  // CAMERA_TOPIC: "/camera/rgb/image_raw/compressed",
+  CAMERA_TOPIC: "/camera//image/compressed",
+  SONAR_TOPIC: "/sonar",
+};
+
+export default Config;
